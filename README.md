28f0cbc28beb8e1d532e7b8950cfa0ee00370217
N1ckorish <n1ckorish14@gmail.com>
2021-11-15 18:06:04 +1100

    submit to github for assessment re-submit

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# professional-readme-generator
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  <img alt="GitHub followers" src="https://img.shields.io/github/followers/N1cholasSmith?style=social">     <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/N1cholasSmith/horiseon-search-engine-optimization?style=social">     <img alt="GitHub commit activity" src="https://img.shields.io/github/commit-activity/w/N1cholasSmith/horiseon-search-engine-optimization?style=social"></img>
+  
+  # Professional README.md Generator
+  # Description
+  The Professional README.md Generator makes filling out a mundane painstaking task easy with automated prompts with the utilization of inquirer
+
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Technologies Utilized](#technologies-ultilized)
+  - [Credits/Contributions](#credits/contributions)
+  - [Future Development](#future-development)
+  - [How To Contribute](#how-to-contribute)
+  - [License](#license)
+  - [Testing Instructions](#testing-instructions)
+
+  # Installation
+  Node.js and Inquirer NPM package are required to receive prompts to fill out your README.md with automation
+
+  # Usage
+  To use this application simply fill out each prompt and it will be displayed in the correlated readme section.
+
+  # Technologies Utilized
+  Technologies utilized in this application are Node.js and Inquirer NPM Package.
+
+  # Credits/Contributions
+  Nicholas Smith Software Engineer
+
+  # Future Development
+  Future Development includes adding more licenses and the capability of adding pictures and videos
+
+  
+  # How to Contribute 
+    Please refer to link for guidelines & Contributor Covenant Code Of Conduct [Contributor Covenant](https://www.contributor-covenant.org/)
+
+  # License 
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    
+  # Testing Instructions
+  Tests are carried out through due diligence of selection each option to test its functionality
+
+  # Questions
+  N1cholasSmith
+  https://github.com/N1cholasSmith
+
+  For further questions please feel free to contact me via email
+  nicholassmithsoftwareengineer@gmail.com
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const inquirer = require("inquirer");
+const inquirer = require("inquirer"); // to generate questions
 const fs = require("fs");
-const generate = require("./utils/generateMarkdown");
 const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 
 inquirer
@@ -10,73 +10,86 @@ inquirer
       type: "input",
       message: "What is the Title for your project?",
       name: "Title",  
+    //   validates to check that the user provided a value
+      validate: (value)=>{if (value){return true} else {return "Please enter a value to continue"}},
          },
     { 
       type: "input",
       message: "Please type a detailed Description of your project?",
-      name: "Description",  
+      name: "Description", 
+      validate: (value)=>{if (value){return true} else {return "Please enter a value to continue"}}, 
          },
     { 
         type: "input",
         message: "How is your application Installed?",
-        name: "Installation",  
-    },
+        name: "Installation",
+        validate: (value)=>{if (value){return true} else {return "Please enter a value to continue"}},  
+        },
     { 
         type: "input",
         message: "How is your application utilized/navigated?",
         name: "Usage",  
+        validate: (value)=>{if (value){return true} else {return "Please enter a value to continue"}},
         },
     { 
         type: "input",
         message: "Please list the Technologies used?",
-        name: "Technologies-Used",  
+        name: "TechnologiesUtilized",  
+        validate: (value)=>{if (value){return true} else {return "Please enter a value to continue"}},
         },
     { 
         type: "input",
         message: "Who contributed and what is their title (Nicholas Smith Software Engineer)",
-        name: "Credits/Contributions",  
+        name: "CreditsContributions",  
+        validate: (value)=>{if (value){return true} else {return "Please enter a value to continue"}},
         },
     { 
         type: "input",
         message: "Future Development Plans/Summary?",
-        name: "Future-Development",  
+        name: "FutureDevelopment",
         },
     { 
-        type: "checkbox",
+        type: "confirm",
         message: "Can others Contribute?",
-        choices: ["yes","no"],
-        name: "How-to-Contribute",  
+        name: "HowToContribute",  
         },
     { 
-        type: "checkbox",
+        type: "list",
         message: "Please type a detailed Description of your project?",
-        choices: ["None",
-        "MIT","Apache License 2.0",
-        "GNU General Public License v3.0",
-        "BSD 2-Clause 'Simplified' License",
-        "BSD 3-Clause 'New' License",
-        "Boost Software License 1.0",
-        "Creative Commons Zero v1.0 Universal",
-        "Eclipse Public License",
-        "GNU Affero General Public License 3.0",
-        "GNU General Public License 2.0",
-        "GNU Lesser General Public License v2.1",
-        "Mozilla Public License 2.0",
-        "The Unlicense"],
+        choices: [
+        {name:"None"},
+        {name:"MIT", value:"MIT"},
+        {name:"APACHE License 2.0", value:"APACHE%20License%202.0"},
+        {name:"GNU General Public License v3.0", value:"GNU%20General%20Public%20License%20v3.0"},
+        {name:"BSD 3-Clause 'New' License", value:"BSD_3-Clause%20'New'%20License"},
+        ],
         name: "License",  
         },
-         
+    {
+        type: "input",
+        message: "Please provide a detailed description of how tests are carried out?",
+        name: "Tests",  
+        },
+        {
+        type: "input",
+        message: "what is your GitHub profile name",
+        name: "GitHub",  
+        },
+        {
+        type: "input",
+        message: "Please provide a link to your GitHub profile",
+        name: "GitHubProfile",  
+            },
+        {
+        type: "input",
+        message: "what is your email",
+        name: "Email",  
+        },
+
 ])
 
 
 .then((response) => {
-    return fs.writeFileSync(path.join (process.cwd(), "README.md"), generate(response));
+    return fs.writeFileSync(path.join (process.cwd(), "README.md"), generateMarkdown(response));
 });
 
-// const generateReadMe = (answers) => (
-
-// )
-
-
-// turns html, css, java into dot points.
-// input.split(“,”).map(tech => tech.trim()).map(cleanTech => ` - ${cleanTech}`)
